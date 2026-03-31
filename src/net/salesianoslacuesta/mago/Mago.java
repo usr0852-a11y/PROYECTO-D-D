8117033cc25cef0015fb4870d7a05f769fd716e5
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-03-31 17:02:29 +0100

    Añado el constructor de mago más el super de la clase personaje

--- src/net/salesianoslacuesta/mago/Mago.java
+++ src/net/salesianoslacuesta/mago/Mago.java
@@ -5,4 +5,10 @@ import net.salesianoslacuesta.personaje.Personaje;
 
 public class Mago extends Personaje {
     private String elemento;
+
+    public Mago(String nombre, int nivel, String elemento) {
+        super(nombre, nivel);
+        this.elemento = elemento;
+    }
+    
 }
