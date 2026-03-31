0b626d6b73f179f4808e97b0227a8f52a49caa17
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-03-31 16:35:51 +0100

    Añado los métodos que nos piden en la clase personaje

--- src/net/salesianoslacuesta/personaje/Personaje.java
+++ src/net/salesianoslacuesta/personaje/Personaje.java
@@ -37,4 +37,42 @@ public void setEnergia(int energia) {
     this.energia = energia;
 }
 
+public void entrenar(int x) {
+    if(x <= 0) {
+        System.out.println("No se puede entrenar 0 o menos niveles.");
+       
+    } else {
+        this.nivel += x;
+    }
+}
+
+public void descansar(int x) {
+    if(x <= 0) {
+        System.out.println("No se puede aumentar 0 o menos energia");
+    } else if (this.energia + x> 100){
+     System.out.println("La energia no puede superar los 100");
+     this.energia = 100;
+    } else {
+        this.energia += x;
+    }
+}
+
+public void gastarEnergia(int x) {
+    this.energia -= x;
+    if (this.energia < 0 ) {
+        this.energia = 0;
+    }
+}
+
+public void mostrarInfo() {
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Nivel: " + nivel);
+    System.out.println("Energia: " + energia);
+
+}
+
+public static void mostrarTotalPersonajes() {
+    System.out.println("Total de personajes: " + totalPersonajes);
+}
+
 }
