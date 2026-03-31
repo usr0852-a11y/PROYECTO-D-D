0732871ffcaf864a13fdd5274c854fdbd6780414
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-03-31 16:46:26 +0100

    Añado el constructor de la clase arma más el super de la clase personaje

--- src/net/salesianoslacuesta/guerrero/Guerrero.java
+++ src/net/salesianoslacuesta/guerrero/Guerrero.java
@@ -6,4 +6,9 @@ import net.salesianoslacuesta.personaje.Personaje;
 public class Guerrero extends Personaje {
 private String arma;
 
+public Guerrero(String nombre, int nivel, String arma) {
+    super(nombre, nivel);
+    this.arma = arma;
+}
+
 }
